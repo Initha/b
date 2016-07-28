b94f9af22f7d4ffb1055266e2c17219071149777
Initha <inithaganesan@gmail.com>
2016-07-28 12:17:50 +0530

    Update and rename Npz.java to F.java

---
+++ F.java
@@ -0,0 +1,16 @@
+import java.io.*;
+	import java.util.scanner;
+	class F
+	{
+	int i,a,fact=1;
+	Scanner in=new Scanner(System.in);
+	System.out.println("enter the value for a");
+	a=in.nextint();
+	for(i=1;i<=n;i++)
+	{
+	fact=fact*i;
+	System.out.println("The factorial of a is "+fact);
+	}
+	}
+	
+Click here to Rep
